2a36d890d62a444cbc9e9b895359f25c6c107068
Barbara Peric <b.peric10@gmail.com>
2019-08-07 15:42:07 +0200

    yelp

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,7 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  validates :name, uniqueness: true, presence: true
+  validates :name, presence: true
   validates :address, presence: true
-  validates :phone_number, presence: true
   validates :category, presence: true, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian']}
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Jamies',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '132 423 432',
+    category:  'italian',
+  },
+  {
+   name:         'Leon',
+    address:      'City, London E2 7JE',
+    phone_number: '132 231 324',
+    category:  'chinese',
+  },
+  {
+   name:         'Itsu',
+    address:      'London Bridge, London E2 7JE',
+    phone_number: '132 564 765',
+    category:  'japanese',
+  },
+  {
+   name:         'Makan Makan',
+    address:      'Shoreditch, London E2 7JE',
+    phone_number: '132 969 045',
+    category:  'french',
+  },
+  {
+   name:         'Geo',
+    address:      'Westminster, London E2 7JE',
+    phone_number: '132 111 555',
+    category:  'belgian',
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
